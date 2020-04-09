d490a7316e42cfa4e7e3fa2ead4c55232b27cb4d
stanarua@aol.com <stanarua@aol.com>
2020-04-09 14:51:41 +0100

    initial commit to test codebase

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const avgIncome = data.region.avgDailyIncomeInUSD
+const currentlyInfected = () => data.reportedCase * 10;
+const infectionsByRequestedTime = () => currentlyInfected * 1024;
+const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
+const hospitalBedsByRequestedTime = () => data.totalHospitalBeds - severeCasesByRequestedTime;
+const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
+const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
+const dollarsInFlight = () => infectionsByRequestedTime * avgIncome * 30
+
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    const avgIncome = data.region.avgDailyIncomeInUSD
+    const currentlyInfected = () => data.reportedCase * 10;
+    const infectionsByRequestedTime = () => currentlyInfected * 1024;
+    const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
+    const hospitalBedsByRequestedTime = () => data.totalHospitalBeds - severeCasesByRequestedTime;
+    const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
+    const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
+    const dollarsInFlight = () => infectionsByRequestedTime * avgIncome * 30
+    return{
+        data: input,
+        impact:{},
+        severeImpact: {}
+    }
+};
 
 export default covid19ImpactEstimator;

71be864c831929c2eeee168d5e6fac429b70dd02
stanarua@aol.com <stanarua@aol.com>
2020-04-09 17:00:24 +0100

    ready to test second attempt

--- src/estimator.js
+++ src/estimator.js
@@ -1,43 +1,40 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    const dollarPerDay =() => {
-
-    }
-    const avgIncome = input.region.avgDailyIncomeInUSD
-    const currentlyInfected = () => input.reportedCase * 10;
-    const currentlySevereInfected = () => input.reportedCase * 50;
-    const infectionsByRequestedTime = () => currentlyInfected * 1024;
-    const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
-    const hospitalBedsByRequestedTime = () => input.totalHospitalBeds - severeCasesByRequestedTime;
-    const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
-    const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
-    const dollarsInFlight = () => {
+  const input = data;
+  const avgIncome = input.region.avgDailyIncomeInUSD;
+  const currentlyInfected = () => input.reportedCase * 10;
+  const currentlySevereInfected = () => input.reportedCase * 50;
+  const infectionsByRequestedTime = () => currentlyInfected * 1024;
+  const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
+  const hospitalBedsByRequestedTime = () => input.totalHospitalBeds - severeCasesByRequestedTime;
+  const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
+  const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
+  const dollarsInFlight = () => {
         if(avgIncome > 1.5){
-            return infectionsByRequestedTime * avgIncome * 30
+            return infectionsByRequestedTime * avgIncome * 30;
         }else{
-            return   (infectionsByRequestedTime * 0.65) * avgIncome * 30
+            return   (infectionsByRequestedTime * 0.65) * avgIncome * 30;
         }
       
     }
-    return{
-        data: input,
-        impact:{
-            currentlyInfected,
-            infectionsByRequestedTime,
-            severeCasesByRequestedTime,
-            hospitalBedsByRequestedTime,
-            casesForICUByRequestedTime,
-            casesForVentilatorsByRequestedTime,
-            dollarsInFlight
+  return{
+    data: input,
+    impact:{
+        currentlyInfected,
+        infectionsByRequestedTime,
+        severeCasesByRequestedTime,
+        hospitalBedsByRequestedTime,
+        casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime,
+        dollarsInFlight
         },
-        severeImpact: {
-            currentlySevereInfected,
-            infectionsByRequestedTime,
-            severeCasesByRequestedTime,
-            hospitalBedsByRequestedTime,
-            casesForICUByRequestedTime,
-            casesForVentilatorsByRequestedTime,
-            dollarsInFlight
+    severeImpact: {
+        currentlySevereInfected,
+        infectionsByRequestedTime,
+        severeCasesByRequestedTime,
+        hospitalBedsByRequestedTime,
+        casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime,
+        dollarsInFlight
         }
     }
 };

511f6bfaa5be753693f56f9ace7c717cc201ba68
stanarua@aol.com <stanarua@aol.com>
2020-04-09 16:44:53 +0100

    ready to test

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,24 @@
 const covid19ImpactEstimator = (data) => {
     const input = data;
+    const dollarPerDay =() => {
+
+    }
     const avgIncome = input.region.avgDailyIncomeInUSD
     const currentlyInfected = () => input.reportedCase * 10;
+    const currentlySevereInfected = () => input.reportedCase * 50;
     const infectionsByRequestedTime = () => currentlyInfected * 1024;
     const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
     const hospitalBedsByRequestedTime = () => input.totalHospitalBeds - severeCasesByRequestedTime;
     const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
     const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
-    const dollarsInFlight = () => infectionsByRequestedTime * avgIncome * 30
+    const dollarsInFlight = () => {
+        if(avgIncome > 1.5){
+            return infectionsByRequestedTime * avgIncome * 30
+        }else{
+            return   (infectionsByRequestedTime * 0.65) * avgIncome * 30
+        }
+      
+    }
     return{
         data: input,
         impact:{
@@ -20,7 +31,7 @@ const covid19ImpactEstimator = (data) => {
             dollarsInFlight
         },
         severeImpact: {
-            currentlyInfected,
+            currentlySevereInfected,
             infectionsByRequestedTime,
             severeCasesByRequestedTime,
             hospitalBedsByRequestedTime,

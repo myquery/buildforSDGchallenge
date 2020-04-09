569b1f648e041308744b523cbb42cafc72d442c3
stanarua@aol.com <stanarua@aol.com>
2020-04-09 15:51:52 +0100

    initial commit from the local repo

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,33 @@
-const avgIncome = data.region.avgDailyIncomeInUSD
-const currentlyInfected = () => data.reportedCase * 10;
-const infectionsByRequestedTime = () => currentlyInfected * 1024;
-const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
-const hospitalBedsByRequestedTime = () => data.totalHospitalBeds - severeCasesByRequestedTime;
-const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
-const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
-const dollarsInFlight = () => infectionsByRequestedTime * avgIncome * 30
-
 const covid19ImpactEstimator = (data) => {
     const input = data;
-    const avgIncome = data.region.avgDailyIncomeInUSD
-    const currentlyInfected = () => data.reportedCase * 10;
+    const avgIncome = input.region.avgDailyIncomeInUSD
+    const currentlyInfected = () => input.reportedCase * 10;
     const infectionsByRequestedTime = () => currentlyInfected * 1024;
     const severeCasesByRequestedTime = () => 0.15 * infectionsByRequestedTime;
-    const hospitalBedsByRequestedTime = () => data.totalHospitalBeds - severeCasesByRequestedTime;
+    const hospitalBedsByRequestedTime = () => input.totalHospitalBeds - severeCasesByRequestedTime;
     const casesForICUByRequestedTime = () => 0.5 * infectionsByRequestedTime;
     const casesForVentilatorsByRequestedTime = () => 0.2 * infectionsByRequestedTime;
     const dollarsInFlight = () => infectionsByRequestedTime * avgIncome * 30
     return{
         data: input,
-        impact:{},
-        severeImpact: {}
+        impact:{
+            currentlyInfected,
+            infectionsByRequestedTime,
+            severeCasesByRequestedTime,
+            hospitalBedsByRequestedTime,
+            casesForICUByRequestedTime,
+            casesForVentilatorsByRequestedTime,
+            dollarsInFlight
+        },
+        severeImpact: {
+            currentlyInfected,
+            infectionsByRequestedTime,
+            severeCasesByRequestedTime,
+            hospitalBedsByRequestedTime,
+            casesForICUByRequestedTime,
+            casesForVentilatorsByRequestedTime,
+            dollarsInFlight
+        }
     }
 };
 

e5d90a11504e8f06a7d8fd47e0f27a7f943eb257
stanarua@aol.com <stanarua@aol.com>
2020-04-10 09:09:33 +0100

    challenge 1 estimator returning NAN

--- src/estimator.js
+++ src/estimator.js
@@ -1,8 +1,6 @@
 // Estimate the number per period of time
 const timeToDays = (timeToElapse, periodType) => {
   switch (periodType) {
-    case 'days':
-      return timeToDays;
     case 'weeks':
       return timeToElapse * 7;
     case 'months':
@@ -12,9 +10,6 @@ const timeToDays = (timeToElapse, periodType) => {
   }
 };
 
-// Generic function to computed currently Infected for impact and sever impact
-const predictInfected = (cases, multiply) => cases * multiply;
-
 const covid19ImpactEstimator = (data) => {
   const output = {
     data,
@@ -23,8 +18,8 @@ const covid19ImpactEstimator = (data) => {
   };
 
   const days = timeToDays(data.timeToElapse, data.periodType);
-  output.impact.currentlyInfected = predictInfected(data.reportedCases * 10);
-  output.severeImpact.currentlyInfected = predictInfected(data.reportedCases * 50);
+  output.impact.currentlyInfected = Math.trunc(data.reportedCases * 10 * 1024);
+  output.severeImpact.currentlyInfected = Math.trunc(data.reportedCases * 50 * 1024);
   // eslint-disable-next-line max-len
   output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** Math.floor(days / 3));
   // eslint-disable-next-line max-len

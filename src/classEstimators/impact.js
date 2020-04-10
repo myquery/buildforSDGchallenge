2903ed742b50c16dfbefd980a8d10f89c97d3315
stanarua@aol.com <stanarua@aol.com>
2020-04-10 22:53:56 +0100

    attemting to compute

--- src/classEstimators/impact.js
+++ src/classEstimators/impact.js
@@ -53,9 +53,10 @@ export default class Impact {
   // Compute value for available beds per hospital
   availableBedsPerHospital() {
     const timed = this.timeToDays();
+    // const occupied = Math.round(this.beds * 0.65);
     // const availableBeds = Math.round(this.beds * 0.95);
     const availableBedsForSevereCases = Math.round(this.beds * 0.35);
-    const hospitalSevereCases = this.severeCases() * (2 ** timed);
+    const hospitalSevereCases = this.severeCases() * timed;
     return (availableBedsForSevereCases - hospitalSevereCases);
   }
 }

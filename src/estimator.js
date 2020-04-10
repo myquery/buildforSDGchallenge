ca8e7315de41eae432ad42cc553e22689a8743a5
stanarua@aol.com <stanarua@aol.com>
2020-04-10 11:54:14 +0100

    expecting estimated severe case to pass test

--- src/estimator.js
+++ src/estimator.js
@@ -24,7 +24,7 @@ const infectedSnapshot = (currInfected, multiplyBydays) => currInfected * (2 ** 
 
 // Compute available per hospital
 // eslint-disable-next-line max-len
-const availableBedsPerHospital = (bedsPerHospital, severeCases, multiplyBydays) => Math.floor(((bedsPerHospital - severeCases) * multiplyBydays) * 0.35);
+const availableBedsPerHospital = (bedsPerHospital, severeCases, multiplyBydays) => Math.floor((bedsPerHospital * 0.35) - (severeCases * multiplyBydays));
 
 // Compute severe cases
 const infectedPerTime = (infected, multiply) => Math.floor(infected * multiply);

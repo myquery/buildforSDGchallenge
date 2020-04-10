8adfa5977a98103620b935c1cb1ce97b50cbaf72
stanarua@aol.com <stanarua@aol.com>
2020-04-10 08:05:42 +0100

    challenge 1 attempts

--- src/estimator.js
+++ src/estimator.js
@@ -9,6 +9,10 @@ const timeToDays = (timeToElapse, periodType) => {
       return timeToElapse;
   }
 };
+
+// Generic function to computed currently Infected for impact and sever impact
+const predictInfected = (cases, multiply) => cases * multiply;
+
 const covid19ImpactEstimator = (data) => {
   const output = {
     data,
@@ -17,8 +21,8 @@ const covid19ImpactEstimator = (data) => {
   };
 
   const days = timeToDays(data.timeToElapse, data.periodType);
-  output.impact.currentlyInfected = data.reportedCases * 10;
-  output.severeImpact.currentlyInfected = data.reportedCases * 50;
+  output.impact.currentlyInfected = predictInfected(data.reportedCases * 10);
+  output.severeImpact.currentlyInfected = predictInfected(data.reportedCases * 50);
   // eslint-disable-next-line max-len
   output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** Math.floor(days / 3));
   // eslint-disable-next-line max-len

a2203f8e2249f51c0e1546514ae86ae3d3d83fd9
stanarua@aol.com <stanarua@aol.com>
2020-04-11 00:46:05 +0100

    computing available hospital bed again......

--- src/classEstimators/impact.js
+++ src/classEstimators/impact.js
@@ -52,9 +52,10 @@ export default class Impact {
   // Compute value for available beds per hospital
   availableBedsPerHospital() {
     const timed = this.timeToDays();
-    const capacity = Math.round(this.beds ? this.beds * 0.90 : this.beds * 0.95);
-    const availableBedsForSevereCases = Math.round(capacity * 0.35);
+    const occupiedBed = Math.round(this.beds * 0.65);
+    // const capacity = Math.round(this.beds ? this.beds * 0.90 : this.beds * 0.95);
+    const availableBedsForSevereCases = Math.round(this.beds * 0.35);
     const hospitalSevereCases = this.severeCases() * (2 ** timed);
-    return availableBedsForSevereCases - hospitalSevereCases;
+    return (occupiedBed + availableBedsForSevereCases) - hospitalSevereCases;
   }
 }

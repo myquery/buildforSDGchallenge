2faf9699201f3a252fea794d35a089242abe42fc
stanarua@aol.com <stanarua@aol.com>
2020-04-11 11:11:40 +0100

    debugging .

--- src/classEstimators/impact.js
+++ src/classEstimators/impact.js
@@ -1,4 +1,4 @@
-export default class Impact {
+class Impact {
   constructor(cases, time, beds, elapseTime) {
     this.cases = cases;
     this.time = time;
@@ -51,11 +51,11 @@ export default class Impact {
 
   // Compute value for available beds per hospital
   availableBedsPerHospital() {
-    // const timed = this.timeToDays();
-    // const occupiedBed = Math.round(this.beds * 0.65);
-    // const capacity = Math.round(this.beds ? this.beds * 0.90 : this.beds * 0.95);
+    const timed = this.timeToDays();
     const availableBedsForSevereCases = this.beds * 0.35;
-    const hospitalSevereCases = this.severeCases();
+    const hospitalSevereCases = this.severeCases() * (2 ** timed);
     return Math.trunc(availableBedsForSevereCases - hospitalSevereCases);
   }
 }
+
+export default Impact;

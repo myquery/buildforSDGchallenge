e9e39fb30ba0db87f2d2c29a8d63465ff562b49e
stanarua@aol.com <stanarua@aol.com>
2020-04-12 12:39:05 +0100

    debugging with different codebase just to test

---
+++ src/estimator-origin.js
@@ -0,0 +1,44 @@
+import Impact from './classEstimators/impact';
+
+
+const covid19ImpactEstimator = (data) => {
+  const {
+    reportedCases, periodType, timeToElapse, totalHospitalBeds
+  } = data;
+
+  const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data;
+
+  const estimator = new Impact(
+    reportedCases,
+    periodType,
+    totalHospitalBeds,
+    timeToElapse,
+    avgDailyIncomeInUSD,
+    avgDailyIncomePopulation
+  );
+  const output = {
+    data,
+    impact: {
+      currentlyInfected: estimator.currentlyInfectedByRegion(10),
+      infectionsByRequestedTime: estimator.infectedImpact(),
+      severeCasesByRequestedTime: estimator.severeCases(),
+      hospitalBedsByRequestedTime: estimator.availableBedsPerHospitalForImpact(),
+      casesForICUByRequestedTime: estimator.impactRequiredICU(),
+      casesForVentilatorsByRequestedTime: estimator.impactCasesRequiredVentilator(),
+      dollarsInFlight: estimator.getDollarsInFlightForImpactCases()
+    },
+    severeImpact: {
+      currentlyInfected: estimator.currentlyInfectedByRegion(50),
+      infectionsByRequestedTime: estimator.infectedSevereImpact(),
+      severeCasesByRequestedTime: estimator.severeCases(),
+      hospitalBedsByRequestedTime: estimator.availableBedsPerHospitalForSevere(),
+      casesForICUByRequestedTime: estimator.severeRequiredICU(),
+      casesForVentilatorsByRequestedTime: estimator.severeCasesRequiredVentilator(),
+      dollarsInFlight: estimator.getDollarsInFlightForSevereCases()
+    }
+  };
+
+  return output;
+};
+
+export default covid19ImpactEstimator;
